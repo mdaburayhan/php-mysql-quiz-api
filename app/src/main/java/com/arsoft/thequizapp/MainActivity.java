b64f2c378642c752d157c1f99736e73e2d387348
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-27 14:21:09 +0600

    Added AndroidManifest.xml permission - configured the MainActivity for getting the first quiz

--- app/src/main/java/com/arsoft/thequizapp/MainActivity.java
+++ app/src/main/java/com/arsoft/thequizapp/MainActivity.java
@@ -7,18 +7,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.arsoft.thequizapp.databinding.ActivityMainBinding;
+import com.arsoft.thequizapp.model.QuestionList;
+import com.arsoft.thequizapp.model.Questions;
+import com.arsoft.thequizapp.viewmodel.QuizViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    QuizViewModel quizViewModel;
+    List<Questions> questionsList;
+
+    static int result = 0;
+    static int totalQuestions = 0;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main);
+
+        // Resetting the scores:
+        result = 0;
+        totalQuestions = 0;
+
+        // Creating an instance of 'QuizViewModel'
+        quizViewModel = new ViewModelProvider(this)
+                .get(QuizViewModel.class);
+
+        // Displaying the First Question
+        displayFirstQuestion();
+
+
+
+
     }
+
+    public void displayFirstQuestion(){
+        // Observing LiveData from a viewmodel
+        quizViewModel.getQuestionListLiveData().observe(
+                this,
+                new Observer<QuestionList>() {
+                    @Override
+                    public void onChanged(QuestionList questions) {
+                        // Called when the data inside LIVEDATA changes
+                        questionsList = questions;
+
+                        binding.txtQuestion.setText("Question 1: "+questions.get(0).getQuestion());
+                        binding.radio1.setText(questions.get(0).getOption1());
+                        binding.radio2.setText(questions.get(0).getOption2());
+                        binding.radio3.setText(questions.get(0).getOption3());
+                        binding.radio4.setText(questions.get(0).getOption4());
+
+                    }
+                }
+        );
+    }
+
 }
